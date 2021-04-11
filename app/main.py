04466e28601091f0c42c34afdc297093fc149132
Kevin TSI <kevtsiii@gmail.com>
2021-04-11 19:03:55 +0200

    some changes to main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,4 @@
 import uvicorn
-<<<<<<< HEAD
-
-from typing import Optional
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-=======
 from fastapi import FastAPI, status, Response
 from .firebase import *
 from .models.models import UserRegister
@@ -17,15 +8,12 @@ from .services.services import UserService
 
 app = FastAPI()
 #load_dotenv(dotenv_path=".env")
->>>>>>> c88763c0ce94aa18d55f82203aa3c9c353cd776a
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-<<<<<<< HEAD
-=======
 @app.post("/register", status_code=status.HTTP_201_CREATED)
 async def register(data : UserRegister, response  : Response):
     code = UserService().create_user(data)
@@ -39,7 +27,6 @@ async def register(data : UserRegister, response  : Response):
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 
 
->>>>>>> c88763c0ce94aa18d55f82203aa3c9c353cd776a
 #@app.get("/items/{item_id}")
 #def read_item(item_id: int, q: Optional[str] = None):
 #    return {"item_id": item_id, "q": q}
